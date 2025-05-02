914a97a25d462012c8a9948887c85fb1b9bbf165
AstorBia <kamarquess@yahoo.com.br>
2025-05-02 10:17:00 -0300

    Inicialização do projeto

---
+++ src/main/java/org/example/yardflow/YardFlowApplication.java
@@ -0,0 +1,13 @@
+package org.example.yardflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YardFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YardFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/yardflow/YardFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yardflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YardFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
